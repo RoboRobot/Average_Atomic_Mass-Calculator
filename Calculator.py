a77dff77b31ff00234dfefd728dccf03068d1d40
RoboRobot <purplepikmin95@gmail.com>
2017-11-24 13:22:48 -0500

    Clarity

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,9 @@
 def Average_Atomic_Mass():
     isotopes = input("How many isotopes?")
     average = 0
-    for x in range(0,int(isotopes)):
-        mass = input("What is the mass in amu?")
+    for x in range(1,int(isotopes) + 1):
+        mass = input("What is the isotope %s's mass in amu?" % x)
         percent = input("How abundant is it in %?")
         average += (float(mass)*(float(percent)/100))
-    print(average)
+    print("%s amu" %average)
 Average_Atomic_Mass()
